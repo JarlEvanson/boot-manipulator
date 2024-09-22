5135d9deafbe6cdd2b208cb06d2a8493140682cb
Jarl Evanson <evanson.jarl@gmail.com>
2024-09-22 17:33:54 -0500

    `boot-manipulator`: Refactor UEFI setup

--- boot-manipulator/src/main.rs
+++ boot-manipulator/src/main.rs
@@ -3,95 +3,77 @@
 #![no_std]
 #![no_main]
 
-use core::{
-    fmt::{self, Write},
-    ptr,
-};
+use core::{fmt, ptr};
 
 use arch::virtualization;
-use uefi::{boot, proto::console::serial::Serial};
 
 mod arch;
 pub mod console;
 mod logging;
 mod spinlock;
 
-static EXIT_SERVICES: spinlock::Spinlock<
-    unsafe extern "efiapi" fn(*mut core::ffi::c_void, usize) -> uefi::Status,
-> = spinlock::Spinlock::new(placeholder);
+static mut EXIT_BOOT_SERVICES_PTR: unsafe extern "efiapi" fn(
+    *mut core::ffi::c_void,
+    usize,
+) -> uefi::Status = placeholder;
 
 #[uefi::entry]
 fn entry_point() -> uefi::Status {
     logging::initialize_logging(log::LevelFilter::Trace);
 
     match setup() {
-        uefi::Status::SUCCESS => {}
-        status_code => {
+        Ok(()) => {}
+        Err(error) => {
+            log::error!("{error}");
             uefi::boot::stall(10_000_000);
-            return status_code;
+            return uefi::Status::LOAD_ERROR;
         }
     }
 
+    log::info!("boot-manipulator successfully loaded");
+
+    uefi::Status::SUCCESS
+}
+
+fn setup() -> Result<(), DriverSetupError> {
+    setup_boot_services_interception();
+
+    virtualization::allocate_basic_memory();
+
+    Ok(())
+}
+
+/// Various errors that can occur while setting up the driver.
+pub enum DriverSetupError {}
+
+impl fmt::Display for DriverSetupError {
+    fn fmt(&self, _: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            _ => Ok(()),
+        }
+    }
+}
+
+fn setup_boot_services_interception() {
     let system_table_ptr = uefi::table::system_table_raw()
         .map(|ptr| ptr.as_ptr())
         .unwrap_or(ptr::null_mut());
 
     let boot_services_table_ptr = unsafe { (*system_table_ptr).boot_services };
-    let exit_boot_services_func_ptr =
-        unsafe { ptr::addr_of_mut!((*boot_services_table_ptr).exit_boot_services) };
+    let exit_boot_services_func = unsafe { &mut ((*boot_services_table_ptr).exit_boot_services) };
 
-    *EXIT_SERVICES.lock() = unsafe { *exit_boot_services_func_ptr };
-    unsafe { *exit_boot_services_func_ptr = exit_boot_services };
-
-    virtualization::allocate_basic_memory();
-
-    uefi::Status::SUCCESS
-}
-
-fn setup() -> uefi::Status {
-    let serial_handle = match boot::get_handle_for_protocol::<Serial>() {
-        Ok(handle) => handle,
-        Err(error) => {
-            let _ = uefi::system::with_stdout(|stdout| {
-                writeln!(stdout, "failed to acquire serial device: {error}")
-            });
-            return uefi::Status::LOAD_ERROR;
-        }
-    };
-    let mut serial = match boot::open_protocol_exclusive::<Serial>(serial_handle) {
-        Ok(protocol) => protocol,
-        Err(error) => {
-            let _ = uefi::system::with_stdout(|stdout| {
-                writeln!(stdout, "failed to open serial protocol: {error}")
-            });
-            return uefi::Status::LOAD_ERROR;
-        }
-    };
-
-    uefi::Status::SUCCESS
-}
-
-struct Debugcon;
-
-impl Write for Debugcon {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        for byte in s.bytes() {
-            unsafe { core::arch::asm!("out dx, al", in("dx") 0xe9, in("al") byte) }
-        }
-
-        Ok(())
-    }
+    unsafe { EXIT_BOOT_SERVICES_PTR = *exit_boot_services_func };
+    *exit_boot_services_func = exit_boot_services;
 }
 
 unsafe extern "efiapi" fn exit_boot_services(
     image_handle: *mut core::ffi::c_void,
     map_key: usize,
 ) -> uefi::Status {
-    let func = *EXIT_SERVICES.lock();
+    let func = unsafe { EXIT_BOOT_SERVICES_PTR };
 
     let result = unsafe { (func)(image_handle, map_key) };
     if result != uefi::Status::SUCCESS {
-        let _ = writeln!(Debugcon, "Exit failed: {result}");
         return result;
     }
 
