7335130d1abb591b012b59d78d61a2bb0fd8ce12
Jarl Evanson <evanson.jarl@gmail.com>
2024-09-21 19:15:03 -0500

    `boot-manipulator`: Create `Console` abstraction

--- boot-manipulator/src/console.rs
+++ boot-manipulator/src/console.rs
@@ -1,1 +1,12 @@
 //! Console manipulation.
+
+use core::error::Error;
+
+trait Console: Send {
+    type ReadError: Error;
+    type WriteError: Error;
+
+    fn read(&mut self, data: &mut [u8]) -> Result<(), (Self::ReadError, usize)>;
+
+    fn write(&mut self, data: &[u8]) -> Result<(), (Self::WriteError, usize)>;
+}
