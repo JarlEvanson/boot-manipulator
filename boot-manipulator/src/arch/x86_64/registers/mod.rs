c4827651dd9fa0ed1fec0c25af2ee8a292f30d3f
Jarl Evanson <evanson.jarl@gmail.com>
2024-09-23 01:26:38 -0500

    `boot-manipulator`: Add `Gdtr` and `Idtr` support

--- boot-manipulator/src/arch/x86_64/registers/mod.rs
+++ boot-manipulator/src/arch/x86_64/registers/mod.rs
@@ -1,4 +1,72 @@
 //! Definitions of interfaces for architectural registers.
 
+use core::mem::MaybeUninit;
+
 pub mod control;
 pub mod msr;
+
+#[repr(C)]
+pub struct Idtr {
+    _padding: [MaybeUninit<u8>; 6],
+    limit: u16,
+    address: u64,
+}
+
+impl Idtr {
+    pub fn get() -> Self {
+        let mut reg = Self::new(0, 0);
+
+        unsafe { core::arch::asm!("sidt [{}]", in(reg) &mut reg) }
+
+        reg
+    }
+
+    pub fn new(address: u64, limit: u16) -> Self {
+        Self {
+            _padding: [MaybeUninit::uninit(); 6],
+            limit,
+            address,
+        }
+    }
+
+    pub fn limit(&self) -> u16 {
+        self.limit
+    }
+
+    pub fn address(&self) -> u64 {
+        self.address
+    }
+}
+
+#[repr(C)]
+pub struct Gdtr {
+    _padding: [MaybeUninit<u8>; 6],
+    limit: u16,
+    address: u64,
+}
+
+impl Gdtr {
+    pub fn get() -> Self {
+        let mut reg = Self::new(0, 0);
+
+        unsafe { core::arch::asm!("sgdt [{}]", in(reg) &mut reg) }
+
+        reg
+    }
+
+    pub fn new(address: u64, limit: u16) -> Self {
+        Self {
+            _padding: [MaybeUninit::uninit(); 6],
+            limit,
+            address,
+        }
+    }
+
+    pub fn limit(&self) -> u16 {
+        self.limit
+    }
+
+    pub fn address(&self) -> u64 {
+        self.address
+    }
+}
