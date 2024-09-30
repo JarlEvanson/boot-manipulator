1f7d32a728544f65bfcf7bbf8f5f1db6a4da9ecc
Jarl Evanson <evanson.jarl@gmail.com>
2024-09-29 22:07:31 -0500

    `boot-manipulator`: Implement `Spinlock` primitive

--- boot-manipulator/src/main.rs
+++ boot-manipulator/src/main.rs
@@ -5,11 +5,14 @@
 
 use uefi::Status;
 
+pub mod spinlock;
+
 #[uefi::entry]
 fn entry_point() -> Status {
     Status::SUCCESS
 }
 
+/// Handles panics occurring in `boot-manipulator`.
 #[cfg_attr(not(test), panic_handler)]
 #[cfg_attr(test, allow(unused))]
 fn panic_handler(_: &core::panic::PanicInfo) -> ! {
