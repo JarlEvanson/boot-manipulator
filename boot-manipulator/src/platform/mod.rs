06346092b66da2ee4c7894640da56f69d1353d2e
Jarl Evanson <evanson.jarl@gmail.com>
2024-10-01 02:19:29 -0500

    `boot-manipulator`: Add processor control API

--- boot-manipulator/src/platform/mod.rs
+++ boot-manipulator/src/platform/mod.rs
@@ -26,6 +26,37 @@ pub trait PlatformOps {
     ///
     /// Returns [`Self::LoggingInitializationError`] if platform logger initialization fails.
     fn initialize_logger() -> Result<&'static dyn log::Log, Self::LoggingInitializationError>;
+
+    /// Returns the identity of the calling processor.
+    ///
+    /// This number is in the range from 0 to [`PlatformOps::get_processor_count()`] - 1.
+    fn processor_identity() -> usize {
+        0
+    }
+
+    /// Returns the total number of logical processors in the system.
+    ///
+    /// # Panics
+    ///
+    /// This function may panic if called on a processor other than the bootstrap processor.
+    fn get_processor_count() -> usize {
+        // Simple implementation pretending there is only 1 processor.
+        1
+    }
+
+    /// Executes the provided `function` on all processors.
+    ///
+    /// # Panics
+    ///
+    /// This function may panic if called on a processor other than the bootstrap processor.
+    /// This function may panic if called in a nested manner.
+    fn execute_on_all_processors(
+        function: fn(*mut core::ffi::c_void),
+        argument: *mut core::ffi::c_void,
+    ) {
+        // Simple implementation pretending there is only 1 processor.
+        function(argument)
+    }
 }
 
 /// Dummy platform to allow for development.
