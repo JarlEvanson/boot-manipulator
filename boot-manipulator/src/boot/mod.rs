7ec0ddd19352108a61263c9b4e3b82fe43065e05
Jarl Evanson <evanson.jarl@gmail.com>
2024-10-05 02:05:26 -0500

    `boot-manipulator`: Add processor control API

--- boot-manipulator/src/boot/mod.rs
+++ boot-manipulator/src/boot/mod.rs
@@ -26,6 +26,29 @@ pub trait BootOps {
     ///
     /// Returns [`Self::LoggingInitializationError`] if boot logger initialization fails.
     fn initialize_logger() -> Result<&'static dyn log::Log, Self::LoggingInitializationError>;
+
+    /// Returns the identity of the calling processor.
+    ///
+    /// The identity is a number from `0..get_processor_count()`.
+    fn processor_identity() -> usize;
+
+    /// Returns the total number of logical processors in the system.
+    ///
+    /// # Panics
+    ///
+    /// This function may panic if called on a processor other than the bootstrap processor.
+    fn get_processor_count() -> usize;
+
+    /// Executes the provided `function` on all processors.
+    ///
+    /// # Panics
+    ///
+    /// This function may panic if called on a processor other than the bootstrap processor.
+    /// This function may panic if called in a nested manner.
+    fn execute_on_all_processors(
+        function: fn(*mut core::ffi::c_void),
+        argument: *mut core::ffi::c_void,
+    );
 }
 
 /// Dummy boot structure to allow for development.
@@ -35,6 +58,18 @@ impl BootOps for DummyBootInterface {
     type LoggingInitializationError = core::fmt::Error;
 
     fn initialize_logger() -> Result<&'static dyn log::Log, Self::LoggingInitializationError> {
+        unimplemented!()
+    }
+
+    fn processor_identity() -> usize {
+        unimplemented!()
+    }
+
+    fn get_processor_count() -> usize {
+        unimplemented!()
+    }
+
+    fn execute_on_all_processors(_: fn(*mut core::ffi::c_void), _: *mut core::ffi::c_void) {
         unimplemented!()
     }
 }
